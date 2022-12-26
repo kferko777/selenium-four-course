d59dd6c512379f011dbb471e6e460af087631d1b
kferko777 <k.ferko777@gmail.com>
2022-12-26 10:15:54 +0100

    added L12_Findelements java class

--- src/test/java/test/L12_FindElements.java
+++ src/test/java/test/L12_FindElements.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -16,14 +17,22 @@ public class L12_FindElements {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
+		//1.Find elements
 		driver.get("https://www.google.com/");
-
 		driver.findElement(By.id("L2AGLb")).click();
-		
+
 		WebElement searchForm = driver.findElement(By.tagName("form"));
 		WebElement searchBox = searchForm.findElement(By.name("q"));
 		searchBox.sendKeys("selenium");
-		
+
+		//2. Get all the elements available with tag name 'select'
+		driver.navigate().to("https://trytestingthis.netlify.app/");
+
+		List<WebElement> elements = driver.findElements(By.tagName("select"));
+
+		for (WebElement element : elements) {
+			System.out.println("Paragraph text:" + element.getText());
+		}
 		driver.close();
 
 	}
