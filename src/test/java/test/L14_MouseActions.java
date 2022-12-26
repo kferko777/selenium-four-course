362c799d75c430a37c8d01dec81740566922aebc
kferko777 <k.ferko777@gmail.com>
2022-12-26 18:06:20 +0100

    update repository

--- src/test/java/test/L14_MouseActions.java
+++ src/test/java/test/L14_MouseActions.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.interactions.Actions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class L13_MouseActions {
+public class L14_MouseActions {
 
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
